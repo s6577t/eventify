01cf94784e263be939676e2a0d39247d2ea6de37
Sam Taylor <sjltaylor@gmail.com>
2011-11-06 16:54:10 +0000

    updated to latest cup

--- build/eventify-0.1.2.js
+++ build/eventify-0.1.2.js
@@ -1,5 +1,4 @@
-/*
-  Copyright (C) 2011 by sjltaylor
+/*  Copyright (C) 2011 by sjltaylor
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
@@ -18,7 +17,6 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
   THE SOFTWARE.
-
 */;/*
 
 parameters: an arbitrary number of arguments, each of which is a valid function name.
